e23ab5e8085cb491badc001d0fb4241a9f53df16
aaaasif <alexasif4gmail.com>
2022-02-23 16:33:01 +0600

    header added

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Container, Nav, Navbar, Spinner } from "react-bootstrap";
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+        <Navbar className="navBar" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand >
+          <img width="120px" src="https://i.ibb.co/vsxmmvr/cover.png" alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link activeClassName="current"  to="/home">
+              Home
+            </Nav.Link>
+
+            <Nav.Link activeClassName="current"  to="/products">
+              Products
+            </Nav.Link>
+            <Nav.Link activeClassName="current"  to="/dashboard">
+              Dashboard
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 };
 
