a908bbe082fb3b51fdbcc7dac878ff028eab434b
aaaasif <alexasif4gmail.com>
2022-02-24 15:16:19 +0600

    Service added

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -3,10 +3,19 @@ import img from "./../../assets/football-website-image/AboutUs.jpg"
 
 const AboutUs = () => {
     return (
-        <div>
+        <div className='d-flex justify-content-between'>
             <div>
-            <img src={img} alt="Paris" width="500" height="300"/>
+            <img className='about-img' src={img} alt="Paris" width="500" height="300"/>
             </div>
+            <div className='aboutUs-text'>
+                <h4>About Us</h4>
+                <h1>We are the best Football Agency</h1>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nam tortor, scelerisque sed diam sodales neque facilisi. Pretium, lorem massa cras duis sollicitudin auctor. Neque viverra rhoncus vitae, quis sit egestas non tortor, ac. Nulla risus, orci congue interdum accumsan condimentum sem. Vulputate consequat orci, dui cum sagittis. Sit sed aenean mauris sodales faucibus vel eu nunc ipsum. Turpis est in mi, pulvinar felis etiam. Ut habitasse ipsum mattis ultrices congue eget sed nec bibendum. Orci nunc, congue amet nisi, magna rhoncus augue tincidunt egestas.
+                </p>
+                <button>Learn More</button>
+            </div>
+
         </div>
     );
 };
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import AboutUs from '../AboutUs/AboutUs';
+// import AboutUs from '../AboutUs/AboutUs';
 // import Banner from '../Banner/Banner';
 import NavBar from '../NavBar/NavBar';
+import Service from '../Service/Service';
 
 const HomePage = () => {
     return (
         <div>
             <NavBar/>
             {/* <Banner/> */}
-            <AboutUs/>
+            {/* <AboutUs/> */}
+            <Service/>
         </div>
     );
 };
