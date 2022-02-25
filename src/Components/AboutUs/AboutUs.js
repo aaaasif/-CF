af03626f684a5cdc7ef6357f80aa02d3ef5184d0
aaaasif <alexasif4gmail.com>
2022-02-25 23:32:51 +0600

    about us updated

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import img from "./../../assets/football-website-image/AboutUs.jpg"
+import { Col, Container, Row } from 'react-bootstrap';
+import aboutLogo from "./../../assets/football-website-image/logo.png"
 
 const AboutUs = () => {
     return (
-        <div className='d-flex justify-content-between'>
-            <div>
-            <img className='about-img' src={img} alt="Paris" width="500" height="300"/>
-            </div>
-            <div className='aboutUs-text'>
-                <h4>About Us</h4>
-                <h1>We are the best Football Agency</h1>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nam tortor, scelerisque sed diam sodales neque facilisi. Pretium, lorem massa cras duis sollicitudin auctor. Neque viverra rhoncus vitae, quis sit egestas non tortor, ac. Nulla risus, orci congue interdum accumsan condimentum sem. Vulputate consequat orci, dui cum sagittis. Sit sed aenean mauris sodales faucibus vel eu nunc ipsum. Turpis est in mi, pulvinar felis etiam. Ut habitasse ipsum mattis ultrices congue eget sed nec bibendum. Orci nunc, congue amet nisi, magna rhoncus augue tincidunt egestas.
-                </p>
-                <button>Learn More</button>
-            </div>
-
-        </div>
+        <Container style={{marginTop:'80px'}}>
+         <Row>
+            <Col className='aboutImage' xs={12} md={6}>
+                    <img src={aboutLogo} alt="" className='aboutLogo'/>
+            </Col>
+            <Col xs={12} md={6}>
+                    <div className='aboutText'>
+                    <h6>About Us</h6>
+                    <h4>We are the best Football <br /> Agency</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nam tortor, scelerisque sed diam sodales neque facilisi. Pretium, lorem massa cras duis sollicitudin auctor. Neque viverra rhoncus vitae, quis sit egestas non tortor, ac. Nulla risus, orci congue interdum accumsan condimentum sem. Vulputate consequat orci, dui cum sagittis. Sit sed aenean mauris sodales faucibus vel eu nunc ipsum. Turpis est in mi, pulvinar felis etiam. Ut habitasse ipsum mattis ultrices congue eget sed nec bibendum. Orci nunc, congue amet nisi, magna rhoncus augue tincidunt egestas.</p>
+                        <button className='learnMoreBtn'>Learn More</button>
+                    </div>
+            </Col>
+         </Row>
+        </Container>
     );
 };
 
